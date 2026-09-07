4ea81ec90652d7f21a8e8ee30f36dfe71f21eeb4
shibinhaq <shibinhaq@gmail.com>
2026-09-07 13:18:11 +0530

    completed

---
+++ experiment2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+#convert celsius into fahrenheit
+c=float(input("enter celsius:"))
+f=c*9/5+32
+print(f"fahrenheit {f}F")
+#convert kilometers to miles
+K=float(input("enter km:"))
+m=K*0.62137
+print(round(m,2),"miles")
+
+
+# In[5]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
